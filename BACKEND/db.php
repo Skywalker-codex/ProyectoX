9a27144d8ea8f90342f4dee7ee19fa92ec8c5712
Sky <127138246+Skywalker-codex@users.noreply.github.com>
2026-09-10 23:43:58 +0200

    a

---
+++ BACKEND/db.php
@@ -0,0 +1,26 @@
+<?php
+
+$server = "SERVIDOR_SQL";
+$database = "Produccion";
+$username = "isaac.esteve";
+$password = "isaac.esteve";
+
+$dsn = "sqlsrv:Server=$server;Database=$database;Encrypt=yes;TrustServerCertificate=no";
+
+try {
+
+    $pdo = new PDO($dsn, $username, $password);
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+} catch (PDOException $e) {
+
+    error_log($e->getMessage());
+
+    http_response_code(500);
+
+    exit("Error interno del servidor");
+}
