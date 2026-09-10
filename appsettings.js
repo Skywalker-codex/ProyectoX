94a3b2462f90b51269fe3537f80061aa85e9c617
Sky <127138246+Skywalker-codex@users.noreply.github.com>
2026-09-10 23:31:46 +0200

    a

--- appsettings.js
+++ appsettings.js
@@ -1,5 +1,5 @@
 {
   "ConnectionStrings": {
-    "DefaultConnection": "Server=localhost;Database=MiAplicacion;Trusted_Connection=True;TrustServerCertificate=True;"
+    "DefaultConnection": "Server=localhost;Database=Produccion;Trusted_Connection=True;TrustServerCertificate=True;"
   }
 }
