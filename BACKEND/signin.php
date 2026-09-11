3a1fd4b085f643bda087772a69fa72434e1712c6
Skywalker-codex <aitorsena242@gmail.com>
2026-09-12 00:46:10 +0200

    a

--- BACKEND/signin.php
+++ BACKEND/signin.php
@@ -38,7 +38,7 @@ try {
         ":password" => $passwordHash
     ]);
 
-    header("Location: ../FRONTEND/PORTAL/portal.html?registro=ok");
+    header("Location: /PORTAL/portal.html?registro=ok");
     exit;
 
 } catch (PDOException $e) {
