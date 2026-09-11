65a2a13c8fa39b5efc50b49edd263d63e1b9732c
Skywalker-codex <aitorsena242@gmail.com>
2026-09-12 00:59:22 +0200

    aaa

--- FRONTEND/login.php
+++ FRONTEND/login.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once __DIR__ . "../BACKEND/db.php";
+require_once "/var/www/ProyectoX/BACKEND/db.php";
 
 $email = $_POST["email"] ?? "";
 $password = $_POST["password"] ?? "";
@@ -12,6 +12,7 @@ $sql = "
     FROM usuarios
     WHERE Email = :email
       AND IsActive = 1
+    LIMIT 1
 ";
 
 $stmt = $pdo->prepare($sql);
@@ -23,7 +24,6 @@ $stmt->execute([
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if (!$user || !password_verify($password, $user["Password"])) {
-
     exit("Usuario o contraseña incorrectos");
 }
 
@@ -32,5 +32,5 @@ session_regenerate_id(true);
 $_SESSION["user_id"] = $user["Id"];
 $_SESSION["email"] = $user["Email"];
 
-header("Location: /portal.html");
+header("Location: /portal");
 exit;
