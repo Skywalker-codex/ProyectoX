8dbc687fc8bff399bc1a5b610a4471b5041573d3
Skywalker-codex <aitorsena242@gmail.com>
2026-09-12 00:03:28 +0200

    m

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-cd /var/www/ProyectoX || exit 1
+echo "===== $(date) ====="
+
+cd /var/www/ProyectoX || {
+    echo "ERROR: no se puede entrar en /var/www/ProyectoX"
+    exit 1
+}
+
+echo "Directorio: $(pwd)"
+echo "Usuario: $(whoami)"
+echo "Git: $(/usr/bin/git --version)"
 
 /usr/bin/git pull origin main
