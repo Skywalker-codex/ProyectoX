4b677c8c220302e1de31cb0e4092de1cad64e94d
Skywalker-codex <aitorsena242@gmail.com>
2026-09-12 00:11:07 +0200

    XD

--- FRONTEND/BACKEND/signin.php
+++ FRONTEND/BACKEND/signin.php
@@ -38,7 +38,7 @@ try {
         ":password" => $passwordHash
     ]);
 
-    header("Location: /FRONTEND/login.html?registro=ok");
+    header("Location: /FRONTEND/PORTAL/portal.html?registro=ok");
     exit;
 
 } catch (PDOException $e) {
